6c430a4f1e71eff2935d6010f4773cc59a7512f3
Jacob Arellano <jmm.arellano@gmail.com>
2021-06-17 11:47:11 -0600

    Add assertions for root path

---
+++ spec/requests/welcome_spec.rb
@@ -0,0 +1,13 @@
+describe "Welcome Page" do
+  describe 'Happy Paths' do
+    it "will send an message telling users to go to READ-ME for endpoints" do
+      get '/'
+      expect(response).to be_successful
+      expect(response).to be_successful
+      welcome = JSON.parse(response.body, symbolize_names: true)
+      expect(welcome).to be_a(Hash)
+      expect(welcome).to have_key(:endpoints)
+      expect(welcome[:endpoints]).to be_a(String)
+    end
+  end
+end
