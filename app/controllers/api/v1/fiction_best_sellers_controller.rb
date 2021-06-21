8dcd427543a2ac7fefea3a5e5aaf7df8a07b6b8c
Jacob Arellano <jmm.arellano@gmail.com>
2021-06-21 14:43:14 -0600

    Add logic to controller

--- app/controllers/api/v1/fiction_best_sellers_controller.rb
+++ app/controllers/api/v1/fiction_best_sellers_controller.rb
@@ -1,6 +1,17 @@
 class Api::V1::FictionBestSellersController < ApplicationController
 
   def index
-    require "pry"; binding.pry
+    books = BookFacade.new(query)
+    if books.valid_params
+      render json: BookSerializer.new(books.search_results)
+    else
+      render json: { error: books.error_message }, status: :bad_request
+    end
+  end
+
+  private
+
+  def query
+    params.permit(:rank_rising, :rank_falling, :weeks_on_list)
   end
 end
