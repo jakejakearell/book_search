22366950a27032bbc88e579e3bb1444ab2e3f167
Jacob Arellano <jmm.arellano@gmail.com>
2021-06-21 11:00:42 -0600

    Happy path assertions for API call

--- spec/requests/api/v1/book_search_request_spec.rb
+++ spec/requests/api/v1/book_search_request_spec.rb
@@ -3,7 +3,7 @@ require "rails_helper"
 RSpec.describe "Fiction book search endpoint" do
 
   describe "Happy Path" do
-    it "returns a current list of the top 10 best sellers w/o queries params" do
+    it "returns a current list of the top 15 best sellers w/o queries params" do
       VCR.use_cassette('happy_path_ficton_no_params') do
         get "/api/v1/fiction_best_sellers"
 
@@ -13,7 +13,7 @@ RSpec.describe "Fiction book search endpoint" do
         expect(body).to be_a(Hash)
         expect(body).to have_key(:data)
         expect(body[:data]).to be_a(Array)
-        expect(body[:data].count).to eq(10)
+        expect(body[:data].count).to eq(15)
 
         body[:data].each do |book|
           expect(book.keys).to eq([:id, :type, :attributes])
@@ -36,41 +36,100 @@ RSpec.describe "Fiction book search endpoint" do
     end
 
     it "can sort books by number of weeks of on the best sellers list " do
-      get "/api/v1/fiction_best_sellers"
+      VCR.use_cassette('happy_path_weeks_on_list') do
+        get "/api/v1/fiction_best_sellers?weeks_on_list=true"
+        body = JSON.parse(response.body, symbolize_names: true)
+        expect(response).to be_successful
+        expect(response.status).to eq(200)
+        expect(body).to be_a(Hash)
+        expect(body).to have_key(:data)
+        expect(body[:data]).to be_a(Array)
+        expect(body[:data].count).to eq(15)
 
-      body = JSON.parse(response.body, symbolize_names: true)
-      expect(response).to be_successful
-      expect(response.status).to eq(200)
-      expect(body).to be_a(Hash)
-      expect(body).to have_key(:data)
-
+        most_weeks_on_chart = body[:data].first[:attributes][:weeks_on_list]
+        body[:data].each do |book|
+          expect(book[:attributes][:weeks_on_list]).to be <= most_weeks_on_chart
+        end
+      end
     end
 
     it "can filter books by those rising up the best sellers list" do
-      get "/api/v1/fiction_best_sellers"
+      VCR.use_cassette('happy_path_rising_on_list') do
+        get "/api/v1/fiction_best_sellers?rank_rising=true"
+        body = JSON.parse(response.body, symbolize_names: true)
+        expect(response).to be_successful
+        expect(response.status).to eq(200)
+        expect(body).to be_a(Hash)
+        expect(body).to have_key(:data)
+        expect(body[:data]).to be_a(Array)
+        expect(body[:data].count).to_not eq(15)
 
-      body = JSON.parse(response.body, symbolize_names: true)
-      expect(response).to be_successful
-      expect(response.status).to eq(200)
-      expect(body).to be_a(Hash)
-      expect(body).to have_key(:data)
+        body[:data].each do |book|
+          book_rank = book[:attributes][:rank]
+          book_rank_last_week = book[:attributes][:previous_rank]
+          book_rank_last_week = 999 if book_rank_last_week == 0
+
+          expect(book_rank).to be < book_rank_last_week
+        end
+      end
     end
 
     it "can filter books by those falling down the best sellers list" do
-      get "/api/v1/fiction_best_sellers"
+      VCR.use_cassette('happy_path_falling_on_list') do
+        get "/api/v1/fiction_best_sellers?rank_falling=true"
+        body = JSON.parse(response.body, symbolize_names: true)
+        expect(response).to be_successful
+        expect(response.status).to eq(200)
+        expect(body).to be_a(Hash)
+        expect(body).to have_key(:data)
+        expect(body[:data]).to be_a(Array)
+        expect(body[:data].count).to_not eq(15)
 
-      body = JSON.parse(response.body, symbolize_names: true)
-      expect(response).to be_successful
-      expect(response.status).to eq(200)
-      expect(body).to be_a(Hash)
-      expect(body).to have_key(:data)
+        body[:data].each do |book|
+          book_rank = book[:attributes][:rank]
+          book_rank_last_week = book[:attributes][:previous_rank]
 
+          expect(book_rank).to be > book_rank_last_week
+        end
+      end
+    end
+
+    it "can filter books and sort the filtered list" do
+      VCR.use_cassette('happy_path_sorted_filtered_list') do
+        get "/api/v1/fiction_best_sellers?rank_falling=true&weeks_on_list=true"
+        body = JSON.parse(response.body, symbolize_names: true)
+        expect(response).to be_successful
+        expect(response.status).to eq(200)
+        expect(body).to be_a(Hash)
+        expect(body).to have_key(:data)
+        expect(body[:data]).to be_a(Array)
+        expect(body[:data].count).to_not eq(15)
+        require "pry"; binding.pry
+        most_weeks_on_chart = body[:data].first[:attributes][:weeks_on_list]
+        body[:data].each do |book|
+          book_rank = book[:attributes][:rank]
+          book_rank_last_week = book[:attributes][:previous_rank]
+
+          expect(book_rank).to be > book_rank_last_week
+          expect(book[:attributes][:weeks_on_list]).to be <= most_weeks_on_chart
+        end
+      end
     end
   end
 
   describe "Sad Path and Edge Case" do
-    it "returns an error if an users asks for rising and falling books" do
-      get "/api/v1/fiction_best_sellers/10000000"
+    xit "returns an error if an users asks for rising and falling books" do
+      get "/api/v1/fiction_best_sellers/rank_falling=true&rank_falling=true"
+
+      data = JSON.parse(response.body, symbolize_names: true)
+
+      expect(response).to_not be_successful
+      expect(response.status).to eq(404)
+
+    end
+
+    xit "returns an error if an users doesn't provide true or false for query" do
+      get "/api/v1/fiction_best_sellers/rank_falling=123"
 
       data = JSON.parse(response.body, symbolize_names: true)
 
